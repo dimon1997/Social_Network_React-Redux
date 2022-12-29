936b8310af35b065943c423e34d011a05bc87d49
Dima <dimalavrinchuk1@ukr.net>
2022-12-29 20:23:33 +0200

    Commit-plagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,65 +1,83 @@
-import axios from "axios";
 import React from "react";
 import s from "./users.module.css";
 import userPhoto from "../../assets/photoUsers/user.png";
+import axios from "axios";
 
-let Users = (props) => {
-  let getUsers = () =>{
-    if(props.users.length === 0){
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items);
+class Users extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
       });
-    
-     }
   }
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
 
-    
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {props.users.map((u) => (
-        <div key={u.id}>
-          <span>
-            <div>
-              <img src={u.photos.small != null ? u.photos.small: userPhoto} alt="don't load" className={s.ava}/>
-            </div>
-            <div>
-                
-             {u.followed ? (
-                <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
-            <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
-            </span>
-            <span>
-              <div>{"u.location.country"}</div>
-              <div>{"u.location.city"}</div>
-            </span>
-          </span>
+  render() {
+    let pagesCount = Math.ceil(
+      this.props.totalUsersCount / this.props.pageSize
+    );
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+      pages.push(i);
+    }
+
+    return (
+      <div>
+        <div>
+          {pages.map((p) => {
+            return (
+              <span
+                className={this.props.currentPage === p && s.selectedPage}
+                onClick={(e) => {this.onPageChanged(p)}}
+                key={p}>
+                {p}
+              </span>
+            );
+          })}
         </div>
-      ))}
-    </div>
-  );
-};
+        {this.props.users.map((u) => (
+          <div key={u.id}>
+            <span>
+              <div>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="don't load" className={s.ava} />
+              </div>
+              <div>
+                {u.followed ? 
+                  (<button onClick={() => {this.props.unfollow(u.id);}}>Unfollow</button>)
+                  : 
+                  (<button onClick={() => {this.props.follow(u.id);}}>Follow</button>)
+                }
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
+              </span>
+            </span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Users;
-
-
-// props.setUsers([
-//   { id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVxS9mLOcvLmWZziaZeB1G0DVbCJpxV_SpUQ&usqp=CAU', followed: false, fullName: "Bob?", status: "I'm a bosss", location:  {city: "Minsk", country: "Belarus"}},
-//   { id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVxS9mLOcvLmWZziaZeB1G0DVbCJpxV_SpUQ&usqp=CAU', followed: true, fullName: "Stiv", status: "I'm a engineer", location:  {city: "Moskow", country: "Russia"}},
-//   { id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVxS9mLOcvLmWZziaZeB1G0DVbCJpxV_SpUQ&usqp=CAU', followed: false, fullName: "Ron", status: "I'm a server", location:  {city: "Kiev", country: "Ukraine"}}
-   
-// ])
--- src/components/redux/users-reducer.js
+++ src/components/redux/users-reducer.js
@@ -1,35 +1,44 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 let initialState = {
-  users: []
+  users: [],
+  pageSize: 10,
+  totalUsersCount: 0,
+  currentPage: 2,
 };
 
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FOLLOW: 
-    return {
+    case FOLLOW:
+      return {
         ...state,
-        users: state.users.map(u => {
-            if(u.id === action.userId){
-                return{...u, followed: true};
-            }
-        return u;
-        })
-    }
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: true };
+          }
+          return u;
+        }),
+      };
     case UNFOLLOW:
-        return {
-            ...state,
-            users: state.users.map(u => {
-                if(u.id === action.userId){
-                    return{...u, followed: false};
-                }
-            return u;
-            })
-        }
+      return {
+        ...state,
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return { ...u, followed: false };
+          }
+          return u;
+        }),
+      };
     case SET_USERS:
-        return {...state, users: [...state.users, ...action.users]};
+      return { ...state, users: action.users};
+    case SET_CURRENT_PAGE:
+      return { ...state, currentPage: action.currentPage };
+      case SET_TOTAL_USERS_COUNT:
+      return { ...state, totalUsersCount: action.count };
     default:
       return state;
   }
@@ -38,21 +47,32 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userId) => {
   return {
     type: FOLLOW,
-    userId
+    userId,
   };
 };
 export const unfollowAC = (userId) => {
   return {
     type: UNFOLLOW,
-    userId
+    userId,
   };
 };
 export const setUsersAC = (users) => {
-    return {
-      type: SET_USERS,
-      users
-    };
+  return {
+    type: SET_USERS,
+    users,
   };
+};
+export const setCurrentPageAC = (currentPage) => {
+  return {
+    type: SET_CURRENT_PAGE,
+    currentPage
+  };
+};
+export const setTotalUsersCountAC = (totalUsersCount) => {
+  return {
+    type: SET_TOTAL_USERS_COUNT,
+    count: totalUsersCount
+  };
+};
 
 export default usersReducer;
-
