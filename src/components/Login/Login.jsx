4d94c9ae3c80346bc0cfcc8233dfb7f7052cd1f6
Dima <dimalavrinchuk1@ukr.net>
2023-01-24 20:21:18 +0200

    Commit-login_logout(_api_)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,21 @@
+import { connect } from "react-redux";
+import { redirect } from "react-router-dom";
 import { Field, reduxForm } from "redux-form";
 import { required } from "../../utils/validators/validators";
 import { Input } from "../common/FormsControls/FormsControl";
+import { login} from "../redux/auth-reducer";
+import s from "./Login.module.css"
+// dimalavrinchuk1@ukr.net
+// 556427556427
 
 const LoginForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div>
-        <Field placeholder={"Login"} name={"login"} component={Input} validate={[required]}/>
+        <Field placeholder={"Email"} name={"email"} component={Input} validate={[required]}/>
       </div>
       <div>
-        <Field placeholder={"Password"} name={"password"} component={Input} validate={[required]}/>
+        <Field placeholder={"Password"} name={"password"} type={"password"} component={Input} validate={[required]}/>
       </div>
       <div>
         <Field type={"checkbox"} name={"rememberMe"} component={Input} /> remember me
@@ -21,17 +27,22 @@ const LoginForm = (props) => {
   );
 };
 
-const Login = () => {
+const Login = (props) => {
     const onSubmit = (formData) =>{
-        console.log(formData);
+       props.login(formData.email, formData.password, formData.rememberMe);
     }
+    if(props.isAuth) return redirect("/profile")
+
   return (
     <div>
-      <h1> Login</h1>
+      <h1 className={s.hederLogin}> Login</h1>
       <LoginReduxForm onSubmit={onSubmit}/>
     </div>
   );
 };
 const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
 
-export default Login;
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth
+})
+export default connect(mapStateToProps, {login})(Login);
--- src/components/redux/auth-reducer.js
+++ src/components/redux/auth-reducer.js
@@ -15,21 +15,22 @@ const authReducer = (state = initialState, action) => {
     case SET_USER_DATA:
       return {
         ...state,
-        ...action.data,
-        isAuth: true
+        ...action.payload,
+        //isAuth: true
       };
     default:
       return state;
   }
 };
 
-export const setAuthUserData = (userId, email, login) => {
+export const setAuthUserData = (userId, email, login, isAuth) => {
   return {
     type: SET_USER_DATA,
-    data: {
+    payload: {
       userId,
       email,
       login,
+      isAuth
     },
   };
 };
@@ -39,7 +40,24 @@ export const getAuthUserData = () => (dispatch) => {
   .then((response) => {
     if(response.data.resultCode === 0){
         let {id, email, login} = response.data.data;
-        dispatch(setAuthUserData(id, email, login));
+        dispatch(setAuthUserData(id, email, login, true));
+    }
+  });
+}
+
+export const login = (email, password, rememberMe) => (dispatch) => {
+  authAPI.login(email, password, rememberMe)
+  .then((response) => {
+    if(response.data.resultCode === 0){
+        dispatch(getAuthUserData());
+    }
+  });
+}
+export const logout = () => (dispatch) => {
+  authAPI.logout()
+  .then((response) => {
+    if(response.data.resultCode === 0){
+      dispatch(setAuthUserData(null, null, null, false));
     }
   });
 }
