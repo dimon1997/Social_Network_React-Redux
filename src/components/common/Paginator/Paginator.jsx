cdc1141cef77038f7d379b13967c3d3e3de76cd4
Dima <dimalavrinchuk1@ukr.net>
2023-02-10 17:50:05 +0200

    Commit-Paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import s from "./Paginator.module.css";
 import { Link} from "react-router-dom";
+import { useState } from "react";
 
-let Paginator = ({onPageChanged, totalUsersCount, pageSize, currentPage}) => {
-  let pagesCount = Math.ceil(totalUsersCount / pageSize);
+let Paginator = ({onPageChanged, totalItemsCount, pageSize, currentPage, portionSize = 10}) => {
+  
+  let pagesCount = Math.ceil(totalItemsCount / pageSize);
 
   let pages = [];
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
 
+ let portionCount = Math.ceil(pagesCount / portionSize);
+ let [portionNumber, setPortionNumber] = useState(1);
+ let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+ let rightPortionPageNumber = portionNumber * portionSize;
+
   return (
-      <div>
-        {pages.map((p) => {
+    <div className={s.paginator}>
+      {portionNumber > 1 && <button onClick={() => {setPortionNumber(portionNumber - 1)}}>back</button>}
+      {pages
+        .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+        .map((p) => {
           return (
             <Link
               className={s.selectedPage}
@@ -26,7 +36,8 @@ let Paginator = ({onPageChanged, totalUsersCount, pageSize, currentPage}) => {
             </Link>
           );
         })}
-      </div>
+      {portionCount > portionNumber && <button onClick={() => {setPortionNumber(portionNumber + 1)}}>next</button>}  
+    </div>
   );
 };
 
