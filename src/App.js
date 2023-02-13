cb1a7551fa7856592802790435fbf8e603a36e40
Dima <dimalavrinchuk1@ukr.net>
2023-02-13 17:35:34 +0200

    Commit-lazy_Loading

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import { Routes, Route } from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import Login from "./components/Login/Login";
-import { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { connect } from "react-redux";
 import { initializeApp } from "./components/redux/app-reducer";
 import Preloader from "./components/common/preloader/Preloader";
+//import { withSuspense } from "./components/hoc/withSuspense";
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
+const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+//import ProfileContainer from "./components/Profile/ProfileContainer";
+const ProfileContainer = React.lazy(()=> import ("./components/Profile/ProfileContainer"));
 
 class App extends Component {
   componentDidMount() {
@@ -31,9 +34,12 @@ class App extends Component {
           <Navbar />
           <div className="app-wrapper-content">
             <Routes>
-              <Route path="/profile/:userId?" element={<ProfileContainer />} />
+              <Route path="" element={ <ProfileContainer />} />
+              <Route path="/profile/:userId?" element={ <Suspense fallback={<div>Loading...</div>}><ProfileContainer /></Suspense>} />
+              {/* <Route path="/profile/:userId?" element={withSuspense(ProfileContainer)} /> */}
               <Route path="/users" element={<UsersContainer />} />
-              <Route path="/dialogs" element={<DialogsContainer />} />
+              <Route path="/dialogs" element={<Suspense fallback={<div>Loading...</div>}><DialogsContainer /></Suspense>} /> 
+              {/* <Route path="/dialogs" element={withSuspense(DialogsContainer)} /> */}
               <Route path="/news" element={<News />} />
               <Route path="/music" element={<Music />} />
               <Route path="/settings" element={<Settings />} />
